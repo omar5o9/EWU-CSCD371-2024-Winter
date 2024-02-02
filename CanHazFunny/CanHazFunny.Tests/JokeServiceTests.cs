79c661996d860c5c5fed7fb19d87ed6ea9013b11
Manny Aguilera <maguilera5@ewu.edu>
2024-02-01 18:37:33 -0800

    Added tests for code coverage

---
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using Xunit;
+using System;
+
+
+namespace CanHazFunny.Tests;
+
+public class JokeServiceTests
+{
+
+    [Fact]
+    public void JokeServiceCreated_IsNotNull()
+    {
+        JokeService service = new JokeService();
+        Assert.NotNull(service);
+    }
+
+}
+
