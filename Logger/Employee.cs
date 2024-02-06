848bf55ead3c217c0599f1e5db58ac166964dc28
Manny Aguilera <maguilera5@ewu.edu>
2024-02-05 22:29:08 -0800

    Tests added

--- Logger/Employee.cs
+++ Logger/Employee.cs
@@ -1,11 +1,12 @@
 ﻿
-namespace Logger
-{
-    public class Employee : Person
-    {
-        public int EmployeeId { get; set; }
-        public string Department { get; set; }
+namespace Logger;
 
-        // Comment: Inheriting from Person to reuse the common code for FirstName, LastName, and FullName.
-    }
+public record Employee : Person
+{
+    public int EmployeeId { get; set; }
+    public string Department { get; set; }
+    public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    // Inheriting from Person to reuse the common code for FirstName, LastName, and FullName.
 }
+
