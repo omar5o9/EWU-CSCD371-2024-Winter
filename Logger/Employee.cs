cdfa38e80282a58a6b917f5dfd678e5f45a2bb6f
Manny Aguilera <maguilera5@ewu.edu>
2024-02-08 20:10:44 -0800

    Added comments to entities

--- Logger/Employee.cs
+++ Logger/Employee.cs
@@ -3,10 +3,13 @@ namespace Logger;
 
 public record Employee : Person
 {
+
+    // EmployeeId and Department are implemented implicitely because they do not
+    // derive from BaseClass
     public int EmployeeId { get; set; }
     public string? Department { get; set; }
     public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-    // Inheriting from Person to reuse the common code for FirstName, LastName, and FullName.
+    // Name is explicit because it derives from Person which derives from BaseClass
 }
 
--- Logger/Student.cs
+++ Logger/Student.cs
@@ -4,9 +4,10 @@ namespace Logger;
 
 public record Student : Person
 {
+    // StudentId is implement implicitely because it does not derive from BaseClass
     public int StudentId { get; set; }
     public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-    // Inheriting from Person to reuse the common code for FirstName, LastName, and FullName.
+    // Name is explicit because it derives from Person which derives from BaseClass
 }
 
