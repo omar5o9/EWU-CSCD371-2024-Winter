64ce5ff8041c9f50e78d76fe6e8c467df200f4e5
Omar Facundo <ofacundo@ewu.edu>
2024-02-12 21:47:45 -0800

    Fixed file structure

---
+++ GenericsHomework/Node.cs
@@ -0,0 +1,76 @@
+﻿namespace GenericsHomework;
+
+public class Node<T>
+{
+
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = this;
+    }
+
+    public void Append(T value)
+    {
+        if(Exists(value))
+        {
+            throw new InvalidOperationException("Value already exists in the list");
+        }   
+        Node<T> newNode = new(value);
+        Node<T> cur = this;
+
+        while (cur.Next != this)
+        {
+            cur = cur.Next;
+        }
+
+        newNode.Next = cur.Next;
+        cur.Next = newNode;
+    }
+
+    public override string ToString()
+    {
+        Node<T> cur = this;
+        string result = "";
+        while (cur.Next != this)
+        {
+            result += cur.Data + " -> ";
+            cur = cur.Next;
+        }
+        result += cur.Data;
+        return result;
+    }
+    
+    public void Clear()
+    {
+        // C# will automatically garbage collect the rest of the nodes
+        Node<T> cur = this;
+        cur.Next = this;
+       
+
+    }
+
+    public bool Exists(T value)
+    {
+
+        Node<T> cur = this;
+        while (cur.Next != this)
+        {
+            if (cur.Data != null && cur.Data.Equals(value))
+            {
+                return true;
+            }
+            cur = cur.Next;
+        }
+        return cur.Data != null && cur.Data.Equals(value);
+        
+        
+    }
+
+
+    
+
+}
+
