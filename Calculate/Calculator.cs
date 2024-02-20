d74a3807d5305f4ae26c497484319d57d9d02b5e
Omar Facundo <ofacundo@ewu.edu>
2024-02-19 20:45:13 -0800

    Fixed file structure

---
+++ Calculate/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Calculate;
+public class Calculator
+{
+
+    public static float Add(int a, int b) => a + b;
+    public static float Subtract(int a, int b) => a - b;
+    public static float Multiply(int a, int b) => a * b;
+    public static float Divide(int a, int b)
+    {
+        if (b == 0)
+        {
+            throw new ArgumentException("Cannot divide by zero");
+        }
+        return (float)a / (float)b;
+    }
+
+        
+    public System.Collections.Generic.IReadOnlyDictionary<char, Func<int, int, float>> MathematicalOperations { get; } =
+        new System.Collections.Generic.Dictionary<char, Func<int, int, float>>
+    {
+        {'+', Add},
+        {'-', Subtract},
+        {'*', Multiply},
+        {'/', Divide}
+    };
+
+        
+    public bool TryCalculate(string input, out float result)
+    {
+        result = 0;
+        string[] parts = input.Split(' ');
+        if (parts.Length != 3) return false;
+        if (!float.TryParse(parts[0], out float a)) return false;
+        if (!float.TryParse(parts[2], out float b)) return false;
+        if (parts[1].Length != 1) return false;
+        if (!MathematicalOperations.TryGetValue(parts[1][0], out Func<int,int,float>? operation)) return false;
+        result = operation((int)a, (int)b);
+        return true;
+    }
+
+
+}
---
+++ Calculate/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculate;
+public class Program
+{
+    
+
+    public Action<string> WriteLine { get; init; } = Console.WriteLine;
+    public Func<string?> ReadLine { get; init; } = Console.ReadLine;
+
+    public Program()
+    {
+
+    }
+
+    public static void Main()
+    {
+       
+    }
+
+
+}
