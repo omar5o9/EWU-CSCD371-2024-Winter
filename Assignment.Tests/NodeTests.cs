44547db13645e666ebff269d3116a0443f3b0e00
Robert Garcia <rgarcia28@ewu.edu>
2024-03-07 20:33:33 -0800

    added node tests

--- Assignment.Tests/NodeTests.cs
+++ Assignment.Tests/NodeTests.cs
@@ -11,4 +11,41 @@ public class NodeTests
         Assert.NotEqual(node.Data, node.Next.Data);
     }
 
+    [Fact]
+    public void Node_ReturnsTrueForExistingValue_Success()
+    {
+        Node<int> node = new(1);
+        node.Append(3);
+        node.Append(5);
+        Assert.True(node.Exists(3));
+    }
+
+    [Fact]
+    public void Node_ReturnsFalseForNonExistingValue_Failure()
+    {
+        Node<int> node = new(1);
+        node.Append(3);
+        node.Append(5);
+        Assert.False(node.Exists(7));
+    }
+
+   
+    [Fact]
+    public void Node_IterateThroughNodes_ReturnsCorrectSequence()
+    {
+        Node<int> node = new(1);
+        node.Append(3);
+        node.Append(5);
+
+        int[] expectedSequence = { 1, 3, 5 };
+        int index = 0;
+
+        foreach (var n in node)
+        {
+            Assert.Equal(expectedSequence[index], n.Data);
+            index++;
+        }
+
+    }
+
 }
