c1588a600dc98a9edeb09b963b4697c291ea51e6
Robert Garcia <rgarcia28@ewu.edu>
2024-03-21 21:58:56 -0700

    nvm

--- Assignment.Tests/PingProcessTests.cs
+++ Assignment.Tests/PingProcessTests.cs
@@ -185,7 +185,7 @@ PING * 56 data bytes
 * packets transmitted, * received, *% packet loss, time *ms
 rtt min/avg/max/mdev = */*/*/* ms
 ".Trim();
-    private void AssertValidPingOutput(int exitCode, string? stdOutput)
+    /*private void AssertValidPingOutput(int exitCode, string? stdOutput)
     {
         Assert.IsFalse(string.IsNullOrWhiteSpace(stdOutput));
         stdOutput = WildcardPattern.NormalizeLineEndings(stdOutput!.Trim());
@@ -193,7 +193,7 @@ rtt min/avg/max/mdev = */*/*/* ms
             $"Output is unexpected: {stdOutput}");
         Assert.AreEqual<int>(0, exitCode);
     }
-    /*private void AssertValidPingOutput(PingResult result) =>
+    private void AssertValidPingOutput(PingResult result) =>
         AssertValidPingOutput(result.ExitCode, result.StdOutput); */
 
 }
